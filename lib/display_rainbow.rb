7693310b8034eb2b82dcdb53e54c81d0735d4279
Tandi Samples <tandisamples@gmail.com>
2019-07-28 15:12:43 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
-colors =['red','orange',]
+colors =['red','orange','yellow',]
 def display_rainbow(colors) 
   puts "R: #{colors['red']}, O: #{colors['orange']}, Y: #{colors['yellow']}, G: #{colors['green']}, B: #{colors['blue']}, I: #{colors['indigo']}, V: #{colors['violet']}""
 end
